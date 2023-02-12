d65441db2fbb10fa82b9a494e10b07587865e549
ChenKong44 <101951249+ChenKong44@users.noreply.github.com>
2023-02-12 15:38:50 -0700

    Update saddle.m

--- leach.V2/leach/function/saddle.m
+++ leach.V2/leach/function/saddle.m
@@ -1,10 +1,25 @@
 function saddle()
-    z = [0 0 0];
+    z = [1 1 1];
     lamda = zeros(3,3);
     theta = [0 0 0];
+
+%     z1 = [0 0 0];
+%     lamda1 = zeros(3,3);
+%     theta1 = [0 0 0];
+% 
+%     z2 = [0 0 0];
+%     lamda2 = zeros(3,3);
+%     theta2 = [0 0 0];
+% 
+%     z3 = [0 0 0];
+%     lamda3 = zeros(3,3);
+%     theta3 = [0 0 0];
+
     target = [2,1,0];
     for t = 1:100
-        [z(1), lamda(1), theta(1)] = some_function(1, target, t, z, lamda, theta);
-        [z(2), lamda(2), theta(2)] = some_function(2, target, t, z, lamda, theta);
-        [z(3), lamda(3), theta(3)] = some_function(3, target, t, z, lamda, theta);
+        [z, lamda, theta] = some_function(1, target, t, z, lamda, theta);
+        [z, lamda, theta] = some_function(2, target, t, z, lamda, theta);
+        [z, lamda, theta] = some_function(3, target, t, z, lamda, theta);
+    end
+
     end
