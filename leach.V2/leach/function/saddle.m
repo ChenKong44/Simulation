a044f2f03cafe6f3f949a294bc851186c0cb06e8
ChenKong44 <101951249+ChenKong44@users.noreply.github.com>
2023-02-13 16:00:26 -0700

    1

--- leach.V2/leach/function/saddle.m
+++ leach.V2/leach/function/saddle.m
@@ -1,7 +1,11 @@
 function saddle()
-    z = [15 18 9];
+    z = [21 8 15];
+    z_spare = [];
+    z_spare2 = [];
+    z_spare3 = [];
     lamda = zeros(3,3);
     theta = [0 0 0];
+    iteration=100;
 
 %     z1 = [0 0 0];
 %     lamda1 = zeros(3,3);
@@ -18,10 +22,33 @@ function saddle()
     target = [2,1,0];
 %     index = 1;
 %     iteration=2;
-    for t = 1:400
-        [z, lamda, theta] = some_function(1, target, t, z, lamda, theta);
-        [z, lamda, theta] = some_function(2, target, t, z, lamda, theta);
-        [z, lamda, theta] = some_function(3, target, t, z, lamda, theta);
+    for t = 1:iteration
+        [z, lamda, target, theta] = some_function(1, target, t, z, lamda, theta);
+        [z, lamda, target, theta] = some_function(2, target, t, z, lamda, theta);
+        [z, lamda, target, theta] = some_function(3, target, t, z, lamda, theta);
+        fprintf('%d %d %d\n',z(1),z(2),z(3));
+        z_spare=[z_spare,z(1)];
+        z_spare2=[z_spare2,z(2)];
+        z_spare3=[z_spare3,z(3)];
     end
-
+        subplot(3,1,1);
+        x=1:1:iteration;
+        
+        % Create plot
+        plot(x,z_spare);
+        
+        % Create xlabel
+        xlabel('Round','FontWeight','bold','FontSize',11,'FontName','Cambria');
+        
+        % Create ylabel
+        ylabel('sum of energy','FontWeight','bold','FontSize',11,...
+            'FontName','Cambria');
+        
+        % Create title
+        title('Sum of energy of nodes vs. round','FontWeight','bold','FontSize',12,...
+            'FontName','Cambria');
+        subplot(3,1,2);
+        plot(x,z_spare2);
+        subplot(3,1,3);
+        plot(x,z_spare3);
     end
--- leach.V2/leach/function/some_function.m
+++ leach.V2/leach/function/some_function.m
@@ -1,6 +1,6 @@
-function [z, lamda, target] = some_function(index, target, iteration, z, lamda, theta)
+function [z, lamda, target, theta] = some_function(index, target, iteration, z, lamda, theta)
     step_size = 0.01;
-    delta = 1e-4;
+    delta = 1e-1;
 
     if target(index) == 0
         return
@@ -15,25 +15,24 @@ function [z, lamda, target] = some_function(index, target, iteration, z, lamda, 
     bitrate = 6;
     ctrPacketLength = 200;
     packetLength = 500;
-    if abs(theta(index) - theta(target(index))) < 0.5 %rssi determination
-        if index == 1
+    if abs(theta(index) - theta(target(index))) < 0.01 %rssi determination
+        fprintf('change node')
+        if index == 1 || index == 2
             target(1) = 3;
-        elseif index == 2
             target(2) = 0;
-        else
-            target(1) = 2;
-            target(2) = 1;
+            target(3) = 1;
         end
 
 %         addpath soil equations
         
-
+        fprintf('%d\n',z(target(index)));
 
         syms x
         L_expect(x) = Energy_init ./ ( ( (x-1)./ x ) .*(Energy_receive+Energy_transfer).* packetLength ./ bitrate+...
             ctrPacketLength.*Energy_transfer./ ( x.* bitrate ) );
         L_expectdiff = diff(L_expect(x));
         L_gradient1 = subs(L_expectdiff,x,z(index));
+
         
 
 %         h_constraint(x) = 6.4 +20.*log(3.*sqrt(x./4./density)./2 )+20.*log(theta(index))+13.035.*sqrt(x./4./density);
@@ -50,7 +49,7 @@ function [z, lamda, target] = some_function(index, target, iteration, z, lamda, 
 
         laplase = L_gradient1 + (lamda(index,target(index)) + lamda(target(index),index)) * h_gradient;
         z_new = z(index) - step_size * laplase;
-        z_new = min(max(z_new,0),20);
+        z_new = round(min(max(z_new,0),30));
     else
         z_new = double(z(index));
     end
@@ -64,6 +63,7 @@ function [z, lamda, target] = some_function(index, target, iteration, z, lamda, 
 
     syms a b
     h_constraint(a,b) = 3./2.*abs(sqrt(a./4./(z(index)./50))-sqrt(b./4./z(target(index))));
+    h_result = subs(h_constraint,{a,b},{z(index),z(target(index))});
     h_constraintdiff = abs(diff(h_constraint(a,b),a));
     h_gradient = subs(h_constraintdiff,{a,b},{z(index),z(target(index))});
 
@@ -76,8 +76,7 @@ function [z, lamda, target] = some_function(index, target, iteration, z, lamda, 
 
     laplase = L_gradient1 + (lamda(index,target(index)) + lamda(target(index),index)) * h_gradient;
     z(index) = z_new - step_size * laplase;
-    z(index) = round(min(max(z(index),0),50));
+    z(index) = round(min(max(z(index),0),30));
 
-    h = 0.05;
-    lamda(index, target(index)) = max((1-step_size^2 * delta)*lamda(index, target(index))+step_size * h, 0);
+    lamda(index, target(index)) = max((1-step_size^2 * delta)*lamda(index, target(index))+step_size * h_result, 0);
 end
