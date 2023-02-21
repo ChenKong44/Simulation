6900b68731e6686e4ad68525f4d8de653e8a2cb1
ChenKong44 <101951249+ChenKong44@users.noreply.github.com>
2023-02-20 19:50:17 -0700

    1

--- leach.V2/leach/untitled.m
+++ leach.V2/leach/untitled.m
@@ -1,9 +1,6 @@
 clc;
 clear;
-xmin=0.05;  %minimum moisture lv
-xmax=0.25;   %max moisture lv
-n=20;
-x=xmin+rand(1,n)*(xmax-xmin);
+
 %     fprintf('theta is: %d\n',theta(index));
 
 max_clustersize = 30;
@@ -13,22 +10,25 @@ density2 = 0.8;
 
 Energy_transfer = 50*0.000000001;
 Energy_receive = 50*0.000000001;
-energy_system = 50*0.0000001;
+energy_system = 50*0.000000001;
 
-br = 5.468;
-ctrPacketLength = 200;
-packetLength = 700;
+br_max = 5.468.*1e3;
+
+ctrPacketLength = 51.*8;
+packetLength = 51.*8;
 
 Energy_init = 0.5;
 
 
-x=1:1:30;
-z=1:1:29;
+x=1:1:100;
+z=1:1:99;
 
 sym x
 % L_expect(x) = (0.4.*x-6).^2+8;
-L_expect(x) = Energy_init ./ ( ( (x-1)./ x ) .*(Energy_receive+Energy_transfer).* packetLength ./ br+...
-    ctrPacketLength.*Energy_transfer./ ( x.* br ) );
+br = (3600 .* 0.01 ./ (1 + 2 .* (x - 1))) .* (125000 ./ (2.^7)) .* (4 ./ (4 + 4./5));
+energycost = ( ( (x-1)./ x ) .*(Energy_receive+Energy_transfer+energy_system).* ctrPacketLength ./ br+...
+    packetLength.*(Energy_transfer+energy_system)./ ( br_max ) );
+L_expect(x) = (Energy_init.*0.01) ./ energycost;
 L_expectdiff = diff(L_expect(x));
 % L_gradient1 = subs(L_expectdiff,x,z);
 
