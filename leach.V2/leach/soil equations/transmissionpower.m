55231e3fcd8859cf0055639609d9600aee35d795
ChenKong44 <101951249+ChenKong44@users.noreply.github.com>
2023-03-09 16:50:53 -0700

    1

--- leach.V2/leach/soil equations/transmissionpower.m
+++ leach.V2/leach/soil equations/transmissionpower.m
@@ -1,4 +1,4 @@
-function [bitrate,Energy_transit_b,Energy_transit_cm] = transmissionpower(basedistance,underground, aboveground,moisture,frequency)
+function [bitrate,Energy_transit_b,Energy_transit_cm,Energy_transit_cm_cm] = transmissionpower(basedistance,underground, aboveground,mean_CMS_distance,moisture,frequency)
 
 %   Input:
 %       prob      cluster size prob
@@ -11,6 +11,9 @@ function [bitrate,Energy_transit_b,Energy_transit_cm] = transmissionpower(basedi
 
     [L_ug, L_ab] = pathloss(underground, aboveground,moisture,frequency);
     L_path_cm = L_ab+ L_ug;
+
+    [L_ug, L_ab] = pathloss(mean_CMS_distance.*0.158, mean_CMS_distance.*0.9,moisture,frequency);
+    L_path_cm_cm = L_ug;
     
     [L_ug, L_ab] = pathloss(basedistance.*0.03, basedistance.*0.97,moisture,frequency);
     L_path_b = L_ab+ L_ug;
@@ -27,5 +30,6 @@ function [bitrate,Energy_transit_b,Energy_transit_cm] = transmissionpower(basedi
     bitrate = (bw ./ (2.^sf)) .* (4 ./ (4 + cr));
     Energy_transit_b = 10.* log10(bw) + Noisefactor + snr- 174 - antennagain+ cableloss+ L_path_b;
     Energy_transit_cm = 10.* log10(bw) + Noisefactor + snr- 174 - antennagain+ cableloss+ L_path_cm;
+    Energy_transit_cm_cm = 10.* log10(bw) + Noisefactor + snr- 174 - antennagain+ cableloss+ L_path_cm_cm;
 
 end
