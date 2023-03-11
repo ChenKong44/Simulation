995a80429d5962d39df4ef44ca67c05a84ef88cc
ChenKong44 <101951249+ChenKong44@users.noreply.github.com>
2023-03-10 17:05:56 -0700

    1

--- leach.V2/leach/function/some_function.m
+++ leach.V2/leach/function/some_function.m
@@ -1,6 +1,6 @@
 function [z, lamda, target, theta,L_result,H_result] = some_function(index, target, iteration, z, lamda, theta,L_result,H_result)
     step_size = 0.08;
-    delta = 1e-3;
+    delta = 1e-1;
     
     if target(index) == 0
         return
